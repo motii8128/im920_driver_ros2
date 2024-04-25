c58c913a004ed1ac65d499615ba5991d6ddfb201
motii8128 <default270robo@gmail.com>
2024-04-26 00:36:04 +0900

    Update

--- include/im920_driver_ros2/im920_driver_ros2.hpp
+++ include/im920_driver_ros2/im920_driver_ros2.hpp
@@ -5,18 +5,27 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/bool.hpp>
 
 namespace im920_driver_ros2
 {
-    class IM920Driver : public rclcpp::Node
+    class IM920DriverROS2 : public rclcpp::Node
     {
         public:
-        explicit IM920Driver(const rclcpp::NodeOptions& options=rclcpp::NodeOptions());
+        explicit IM920DriverROS2(const rclcpp::NodeOptions& options=rclcpp::NodeOptions());
 
         void write_callback(const std_msgs::msg::String::SharedPtr msg);
         void read_callback();
 
-        
+        private:
+        std::string port_name_;
+        int baud_rate;
+        bool enable_write_log_, enable_read_log_;
+        std::shared_ptr<im920_driver_ros2::IM920Serial> im920_serial_;
+
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
     };
 }
 #endif
---
+++ src/im920_driver_ros2.cpp
@@ -0,0 +1,9 @@
+#include "im920_driver_ros2.hpp"
+
+namespace im920_driver_ros2
+{
+    IM920DriverROS2::IM920DriverROS2(const rclcpp::NodeOptions& options):rclcpp::Node("im920_driver_ros2_node", options)
+    {
+        
+    }
+}
