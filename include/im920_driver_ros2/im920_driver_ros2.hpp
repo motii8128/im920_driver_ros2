2d75abb6a674d1a80085e067516bcae6bef5d016
motii <108280115+motii8128@users.noreply.github.com>
2024-04-26 00:04:50 +0900

    Add files via upload

---
+++ include/im920_driver_ros2/im920_driver_ros2.hpp
@@ -0,0 +1,13 @@
+#ifndef IM920_DRIVER_ROS2_HPP_
+#define IM920_DRIVER_ROS2_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace im920_driver_ros2
+{
+    class IM920Driver : public rclcpp::Node
+    {
+        
+    }
+}
+#endif
